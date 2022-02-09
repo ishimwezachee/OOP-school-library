26643b76cdebaffbd1e67a05de539a96deb190e8
ishimwezachee <zacheeishimwe@gmail.com>
2022-02-09 15:07:04 +0200

    fix linters

--- person.rb
+++ person.rb
@@ -15,13 +15,14 @@ class Person
   def can_use_services?
     of_age? || parent_permission
   end
+
   def validate_name
     @name = @corrector.correct_name(name)
   end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
-

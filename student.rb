60d2d7b7f8138b66163d531a0bed9c319d23df2d
ishimwezachee <zacheeishimwe@gmail.com>
2022-02-09 13:07:19 +0200

    fix linters

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, _name = 'unknown', _parent_permission: true)
+    super(age, _name = 'unknown', _parent_permission: true)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require './person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(specialization, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(specialization, age, _name = 'unknown', _parent_permission: true)
+    super(age, _name = 'unknown', _parent_permission: true)
     @specialization = specialization
   end
 

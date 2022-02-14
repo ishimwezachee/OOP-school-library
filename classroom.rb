92f6b7d0e854e0d76abc8562ffc657f671f34e79
ishimwezachee <zacheeishimwe@gmail.com>
2022-02-14 19:14:00 +0200

    Add items in file and list them

--- classroom.rb
+++ classroom.rb
@@ -1,16 +1,15 @@
-require './student'
-require './person'
-class Classroom
-  attr_accessor :label
-  attr_reader :students
+require_relative 'student'
 
-  def initialize(label)
+class Classroom
+  attr_accessor :label, :students
+
+  def intitialize(label)
     @label = label
     @students = []
   end
 
-  def add_students(student)
-    @students.push(student)
+  def add_student(student)
+    @student.push(student) unless @students.include?(student)
     student.classroom = self
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,13 +1,13 @@
-require_relative './person'
-
 class Rental
   attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
-    @person = person
-    person.rental << self
+
     @book = book
     book.rentals << self
+
+    @person = person
+    person.rentals << self
   end
 end
